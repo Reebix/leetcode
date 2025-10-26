9cc4c4eb8f617f89ceb7e80c774fb61c9e82e6cf
Reebix <timon@set-richter.de>
2025-10-26 19:21:29 +0100

    fix: used dedup instead

--- src/remove_duplicates_from_sorted_array.rs
+++ src/remove_duplicates_from_sorted_array.rs
@@ -1,14 +1,6 @@
-use std::collections::HashSet;
-
 impl Solution {
     pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
-        let mut seen: HashSet<i32> = HashSet::new();
-        nums.retain(|num| {
-            let contains = seen.contains(num);
-            seen.insert(*num);
-            !contains
-        });
-
+        nums.dedup();
         nums.len() as i32
     }
 }
