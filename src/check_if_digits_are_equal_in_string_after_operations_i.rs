e49e28509f1f29b66c423afc2d9fd503ed73dd34
Reebix <timon@set-richter.de>
2025-11-24 01:34:16 +0100

    fix: check_if_digits_are_equal_in_string_after_operations_i.rs
    Not totally dumb version

--- src/check_if_digits_are_equal_in_string_after_operations_i.rs
+++ src/check_if_digits_are_equal_in_string_after_operations_i.rs
@@ -1,15 +1,15 @@
 impl Solution {
     pub fn has_same_digits(mut s: String) -> bool {
-        let mut out = "".to_string();
-        while s.len() != 2 {
-            out.clear();
-            for i in 0..s.len() - 1 {
-                out.push(((s.chars().collect::<Vec<_>>()[i].to_digit(10).unwrap() + s.chars().collect::<Vec<_>>()[i + 1].to_digit(10).unwrap()) % 10).to_string().chars().next().unwrap());
+        let mut s = s.into_bytes();
+        for byte in &mut s { *byte -= b'0' }
+        for z in (2..s.len()).rev() {
+            for i in 0..z {
+                s[i] = (s[i] + s[i + 1]) % 10;
             }
-            s = out.to_string();
         }
 
-        s.chars().collect::<Vec<_>>()[0] == s.chars().collect::<Vec<_>>()[1]
+
+        s[0] == s[1]
     }
 }
 
